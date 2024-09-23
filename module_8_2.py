b4f53a624ffef8d9aa0fbd9c2d6dc9b1b71d7293
tmeALF <m1160955@yandex.ru>
2024-09-23 19:54:03 +0300

    Add files via upload

---
+++ module_8_2.py
@@ -0,0 +1,28 @@
+def personal_sum(numbers):
+    result = 0
+    incorrect_data = 0
+    for num in numbers:
+        try:
+            result += float(num)
+        except (ValueError, TypeError):
+            incorrect_data += 1
+            print(f"Некорректный тип данных для подсчёта суммы - {num}")
+    return result, incorrect_data
+def calculate_average(numbers):
+    try:
+        if not isinstance(numbers, (list, tuple, str)):
+            print("В numbers записан некорректный тип данных")
+            return None
+        total, incorrect_data = personal_sum(numbers)
+        if total == 0 and incorrect_data == len(numbers):
+            return 0
+        average = total / (len(numbers) - incorrect_data)
+        return average
+    except ZeroDivisionError:
+        return 0
+
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}')
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')
+print(f'Результат 3: {calculate_average(567)}')
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')
