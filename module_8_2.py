f55bb08f99da061880956ee37a8dcb609199776f
tmeALF <m1160955@yandex.ru>
2024-09-23 22:14:40 +0300

    Add files via upload

--- module_8_2.py
+++ module_8_2.py
@@ -3,11 +3,13 @@ def personal_sum(numbers):
     incorrect_data = 0
     for num in numbers:
         try:
-            result += float(num)
-        except (ValueError, TypeError):
+            result += num
+        except TypeError:
             incorrect_data += 1
             print(f"Некорректный тип данных для подсчёта суммы - {num}")
     return result, incorrect_data
+
+
 def calculate_average(numbers):
     try:
         if not isinstance(numbers, (list, tuple, str)):
@@ -22,7 +24,7 @@ def calculate_average(numbers):
         return 0
 
 
-print(f'Результат 1: {calculate_average("1, 2, 3")}')
-print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')
-print(f'Результат 3: {calculate_average(567)}')
-print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')
+print(f'Результат 1: {calculate_average("1, 2, 3")}')  # Строка перебирается, но каждый символ - строковый тип
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')  # Учитываются только 1 и 3
+print(f'Результат 3: {calculate_average(567)}')  # Передана не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')  # Всё должно работать
